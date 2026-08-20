6b243422d8276b95af7900614a961075384ed2f4
unknown <aldrydianreno212@gmail.com>
2026-08-20 21:58:58 +0700

    mac-button

--- src/pages/Mac.jsx
+++ src/pages/Mac.jsx
@@ -52,21 +52,34 @@ export default function App() {
       </section>
       <div className="h-16 bg-linear-to-b from-white to-gray-100"></div>
 
-      <section className="card-mac-section h-200 gap-5 justify-center items-center bg-gray-100 flex">
+      <section className="h-200 w-full bg-gray-100"></section>
+      <div className="h-16 bg-linear-to-b from-gray-100 to-white"></div>
+
+      <section className="card-mac-section h-200 gap-5 justify-center items-center white flex">
         <div className="card-mac h-150 w-100 flex flex-col bg-white rounded-lg">
-          <img src={mac1} alt="" className="h-100 w-full rounded-lg" />
+          <img src={mac1} alt="" className="h-100 w-full" />
           <h1 className="p-5 text-2xl font-semibold">MacBook Neo</h1>
+          <button className="flex justify-center items-center p-5 mx-5 bg-black h-10 w-30 rounded-full text-white">
+            buy now
+          </button>
         </div>
-        <div className="card-mac flex flex-col h-150 w-100 bg-white rounded-lg">
-          <img src={mac2} alt="" className="h-100 w-full rounded-lg" />
+        <div className="card-mac flex flex-col h-150 w-100 bg-white ">
+          <img src={mac2} alt="" className="h-100 w-full " />
           <h1 className="p-5 text-2xl font-semibold">
             {" "}
             MacBook Air 13" and 15"
           </h1>
+          <button className="flex justify-center items-center p-5 mx-5 bg-black h-10 w-30 rounded-full text-white">
+            buy now
+          </button>
         </div>
         <div className="card-mac flex flex-col h-150 w-100 bg-white rounded-lg">
-          <img src={mac3} alt="" className="h-100 w-full rounded-lg" />
+          <img src={mac3} alt="" className="h-100 w-full " />
           <h1 className="p-5 text-2xl font-semibold">MacBook Pro 14 dan 16"</h1>
+
+          <button className="flex justify-center items-center p-5 mx-5 bg-black h-10 w-30 rounded-full text-white">
+            buy now
+          </button>
         </div>
       </section>
     </main>
