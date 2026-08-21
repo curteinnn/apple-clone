880a26966478473a640782450bcf518c35de3b95
unknown <aldrydianreno212@gmail.com>
2026-08-21 21:36:04 +0700

    entertainment-section

--- src/pages/Entertainment.jsx
+++ src/pages/Entertainment.jsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
     <main>
-      <section className="flex h-200 w-full bg-white"></section>
+      <section className="flex gap-10 justify-center items-center h-200 w-full bg-white">
+        <div className="h-100 w-50 bg-gray-100"></div>
+        <div className="h-100 w-50 bg-gray-100"></div>
+        <div className="h-100 w-50 bg-gray-100"></div>
+        <div className="h-100 w-50 bg-gray-100"></div>
+      </section>
     </main>
   );
 }
