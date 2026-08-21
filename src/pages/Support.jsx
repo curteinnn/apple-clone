ca5d8a4138a0880fed7b0d50c7d7c6898f1ef931
unknown <aldrydianreno212@gmail.com>
2026-08-21 21:38:37 +0700

    support-section

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
     <main>
-      <section className="flex h-200 w-full bg-white"></section>
+      <section className="flex justify-center items-center gap-10 h-200 w-full bg-white">
+        <div className="h-100 bg-black w-100"></div>
+        <div className="h-100 bg-black w-100"></div>
+        <div className="h-100 bg-black w-100"></div>
+      </section>
     </main>
   );
 }
