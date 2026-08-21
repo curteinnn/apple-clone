01b70d3d53bbd4b71c4ef498cac8c25e2be26746
unknown <aldrydianreno212@gmail.com>
2026-08-21 21:33:12 +0700

    accessories-section

--- src/pages/Accessories.jsx
+++ src/pages/Accessories.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
     <main>
-      <section className="flex bg-white h-200 w-full"></section>
+      <section className="flex justify-center items-center bg-white h-screen w-full gap-10">
+        <div className=" h-120 w-100 bg-black rounded-lg hover:bg-white"></div>
+        <div className=" h-120 w-100 bg-gray-100 rounded-lg hover:bg-black"></div>
+      </section>
     </main>
   );
 }
