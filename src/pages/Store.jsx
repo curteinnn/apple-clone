11efffa68377f2863b53daa18152d71419e8144d
unknown <aldrydianreno212@gmail.com>
2026-08-22 21:40:04 +0700

    add-element

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
     <main>
-      <section className="h-200 bg-white"></section>
+      <section className="h-200 bg-white flex gap-5 justify-center items-center">
+        <div className="h-100 w-100"></div>
+        <div className="h-100 w-100"></div>
+        <div className="h-100 w-100"></div>
+      </section>
     </main>
   );
 }
