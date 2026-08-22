b44865a8a76e678453c1cc1e92ea5d3f19aca69f
unknown <aldrydianreno212@gmail.com>
2026-08-22 21:41:42 +0700

    rotate

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -3,7 +3,7 @@ export default function App() {
     <main>
       <section className="h-200 bg-white flex gap-5 justify-center items-center">
         <div className="h-100 w-100 bg-black hover:rotate-12"></div>
-        <div className="h-100 w-100 bg-black"></div>
+        <div className="h-100 w-100 bg-black hover:rotate-45"></div>
         <div className="h-100 w-100 bg-black"></div>
       </section>
     </main>
